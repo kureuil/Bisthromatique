4d084706f8262594a37b6a37e43980332d0ec71c
Brendan Rius <brendan.rius@epitech.eu>
2014-10-27 18:30:20 +0100

    fixed a bug, bug the pervious bug "--3" is back

--- shuntingyard.c
+++ shuntingyard.c
@@ -5,7 +5,7 @@
 ** Login   <rius_b@epitech.net>
 ** 
 ** Started on  Mon Oct 27 15:59:15 2014 brendan rius
-** Last update Mon Oct 27 18:24:19 2014 brendan rius
+** Last update Mon Oct 27 18:29:20 2014 brendan rius
 */
 
 #include <stdlib.h>
@@ -33,7 +33,7 @@ void		handle_operator(t_token *operator,
 
   t = (t_token *) top(*stack);
   while  (t != NULL && is_operator(t) &&
-	  t->value.operator.precedence > operator->value.operator.precedence)
+	  t->value.operator.precedence >= operator->value.operator.precedence)
     {
       stack_to_queue(queue, stack);
       t = (t_token *) top(*stack);
