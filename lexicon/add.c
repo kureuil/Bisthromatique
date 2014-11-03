82e7f722908b986e5664c4d6cf65237131331afc
Louis Person <louis.person@epitech.eu>
2014-11-03 15:07:25 +0100

    Normalize add.c & sub.c

--- lexicon/add.c
+++ lexicon/add.c
@@ -5,7 +5,7 @@
 ** Login   <rius_b@epitech.net>
 ** 
 ** Started on  Mon Oct 27 15:55:31 2014 brendan rius
-** Last update Mon Nov  3 14:49:43 2014 Louis Person
+** Last update Mon Nov  3 14:58:46 2014 Louis Person
 */
 
 #include <stdlib.h>
@@ -16,15 +16,41 @@
 #include "bm_base.h"
 #include "my.h"
 
-t_rcode		action_add(t_base *base,
-			   t_token *n1,
-			   t_token *n2,
-			   t_token *res)
+t_rcode		action_add_compute(t_base *base,
+				   t_token *n1,
+				   t_token *n2,
+				   t_token *res)
 {
   int		carry;
   int		cursor;
   int		shift_n1_n2;
   int		tmp;
+
+  if (n2->size > n1->size)
+    reorder_tokens(&n1, &n2);
+  res->size = n1->size + 1;
+  if (!(res->string_value = res->real_address = malloc(res->size)))
+    return (COULD_NOT_MALLOC);
+  res->string_value[0] = base->string[0];
+  carry = 0;
+  cursor = n1->size - 1;
+  shift_n1_n2 = n1->size - n2->size;
+  while (cursor >= 0 || carry)
+    {
+      tmp = (get_value_at_index(base, n1->string_value, cursor) + carry +
+	     get_value_at_index(base, n2->string_value, cursor - shift_n1_n2));
+      carry = tmp >= base->size ? 1 : 0;
+      res->string_value[cursor-- + 1] = base->string[tmp % base->size];
+    }
+  clean_number_str(base, res);
+  return (OK);
+}
+
+t_rcode		action_add(t_base *base,
+			   t_token *n1,
+			   t_token *n2,
+			   t_token *res)
+{
   t_rcode	ret;
 
   if (n1->sign == NEGATIVE && n2->sign == POSITIVE)
@@ -43,26 +69,9 @@ t_rcode		action_add(t_base *base,
       return (OK);
     }
   if (n1->sign == NEGATIVE && n2->sign == NEGATIVE)
-    {
       res->sign = NEGATIVE;
-    }
-  if (n2->size > n1->size)
-    reorder_tokens(&n1, &n2);
-  res->size = n1->size + 1;
-  if (!(res->string_value = res->real_address = malloc(res->size)))
-    return (COULD_NOT_MALLOC);
-  res->string_value[0] = base->string[0];
-  carry = 0;
-  cursor = n1->size - 1;
-  shift_n1_n2 = n1->size - n2->size;
-  while (cursor >= 0 || carry)
-    {
-      tmp = (get_value_at_index(base, n1->string_value, cursor) + carry +
-	     get_value_at_index(base, n2->string_value, cursor - shift_n1_n2));
-      carry = tmp >= base->size ? 1 : 0;
-      res->string_value[cursor-- + 1] = base->string[tmp % base->size];
-    }
-  clean_number_str(base, res);
+  if ((ret = action_add_compute(base, n1, n2, res)) != OK)
+    return (ret);
   bm_free_token(n1);
   bm_free_token(n2);
   return (OK);
--- lexicon/sub.c
+++ lexicon/sub.c
@@ -5,7 +5,7 @@
 ** Login   <rius_b@epitech.net>
 ** 
 ** Started on  Mon Oct 27 15:56:36 2014 brendan rius
-** Last update Mon Nov  3 14:39:06 2014 Louis Person
+** Last update Mon Nov  3 15:06:47 2014 Louis Person
 */
 
 #include <stdlib.h>
@@ -16,31 +16,14 @@
 #include "operators.h"
 #include "my.h"
 
-t_rcode		action_sub(t_base *base,
-			   t_token *n1,
-			   t_token *n2,
-			   t_token *res)
+t_rcode		action_sub_compute(t_base *base,
+				   t_token *n1,
+				   t_token *n2,
+				   t_token *res)
 {
   t_token	*tmp;
   t_rcode	ret;
-
-  if (n1->sign == POSITIVE && n2->sign == NEGATIVE)
-    return ((ret = action_add(base, n1, n2, res)) != OK ? ret : OK);
-  if (n1->sign == NEGATIVE && n2->sign == POSITIVE)
-    {
-      n1->sign = POSITIVE;
-      if ((ret = action_add(base, n1, n2, res)) != OK)
-	return (ret);
-      res->sign = NEGATIVE;
-      return (OK);
-    }
-  if (n1->sign == NEGATIVE && n2->sign == NEGATIVE)
-    {
-      n2->sign = POSITIVE;
-      if ((ret = action_add(base, n1, n2, res)) != OK)
-	return (ret);
-      return (OK);
-    }
+  
   if (n1->size < n2->size || my_strcmp_base(n1, n2, base) < 0)
     {
       reorder_tokens(&n1, &n2);
@@ -55,6 +38,34 @@ t_rcode		action_sub(t_base *base,
   if ((ret = get_complementary_number(n1, base, res)) != OK)
     return (ret);
   clean_number_str(base, res);
+  return (OK);
+}
+
+t_rcode		action_sub(t_base *base,
+			   t_token *n1,
+			   t_token *n2,
+			   t_token *res)
+{
+  t_rcode	ret;
+
+  if (n1->sign == NEGATIVE && n2->sign == POSITIVE)
+    {
+      n1->sign = POSITIVE;
+      if ((ret = action_add(base, n1, n2, res)) != OK)
+	return (ret);
+      res->sign = NEGATIVE;
+      return (OK);
+    }
+  if ((n1->sign == NEGATIVE && n2->sign == NEGATIVE) ||
+      (n1->sign == POSITIVE && n2->sign == NEGATIVE))
+    {
+      n2->sign = POSITIVE;
+      if ((ret = action_add(base, n1, n2, res)) != OK)
+	return (ret);
+      return (OK);
+    }
+  if ((ret = action_sub_compute(base, n1, n2, res)) != OK)
+    return (ret);
   bm_free_token(n1);
   bm_free_token(n2);
   return (OK);
