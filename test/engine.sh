a088547134a169a966a455aa39884ca0ad17f3b2
Brendan Rius <brendan.rius@epitech.eu>
2014-11-04 12:16:54 +0100

    added scripts for testing

---
+++ test/engine.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+cmds="$( cd $(dirname ${BASH_SOURCE[0]}) && pwd)/testscases/*.cmd"
+
+if [ ! -f $(pwd)/calc ]
+then
+    echo "Binary not found. Aborted."
+    exit 42
+fi
+
+if [ $? -ne 0 ]
+then
+    echo "No tests found. Aborted."
+    exit 42
+fi
+
+for i in $cmds; do
+    res=${i/.cmd/.res}
+    if [ ! -f $res ]
+    then
+	echo "Result file not found. Aborted."
+	exit 42
+    fi
+    base=$(sed '1q;d' $i)
+    size=$(sed '2q;d' $i)
+    ops=$(sed '3q;d' $i)
+    calc=$(sed -n '4,$ p' $i)
+    tmp=$(mktemp --suffix '_bistro')
+    echo $calc | $(pwd)/calc $base $ops $size > $tmp
+    diff=$(diff -y $res $tmp)
+    if [ $? -ne 0 ]
+    then
+	echo -e "-----\n\e[91mFAILED\e[0m\n"
+	echo -e "calc\t->\t$calc\n"
+	echo -e $diff
+	exit 42
+    else
+	echo -e "\e[92mOK\e[0m"
+    fi
+done
+exit 0
---
+++ test/gen.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3.4
+# -*- coding: utf-8 -*-
+
+import random
+import sys
+import uuid
+import os
+from subprocess import *
+
+if len(sys.argv) < 2:
+    size = 1000
+else:
+    size = int(sys.argv[1])
+
+ops = ["+", "-"]
+rops = ["(", ")", "+", "-", "*", "/", "%"]
+for i in range (0, size):
+    name = str(uuid.uuid4())
+    fcmd = open(os.path.dirname(os.path.realpath(__file__)) + "/testscases/" + name + ".cmd", "w")
+    fres = open(os.path.dirname(os.path.realpath(__file__)) + "/testscases/" + name + ".res", "w")
+    s = ""
+    l = random.randint(1, 100)
+    for i in range(0, l):
+        s += str(int(random.getrandbits(random.randint(1, 200)))) + random.sample(ops, 1)[0]
+    s += str(int(random.getrandbits(random.randint(1, 200))))
+    res_bc = check_output("echo \"{0}\" | bc".format(s), shell = True)
+    fcmd.write("0123456789" + '\n')
+    fcmd.write(str(len(s)) + '\n')
+    fcmd.write(''.join(rops) + '\n')
+    fcmd.write(s + '\n')
+    fres.write(res_bc.decode('utf-8'));
+    fcmd.close()
+    fres.close()
